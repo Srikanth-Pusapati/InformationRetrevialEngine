60a275e487d4a20bb67ffeb7c41d8ed7152d7bf7
Srikanth <srikanthpusapati@my.unt.edu>
2017-04-02 11:31:41 -0500

    Code Update for assignment 2

--- IrsProject/src/ReadInputFiles.java
+++ IrsProject/src/ReadInputFiles.java
@@ -6,39 +6,74 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.text.Collator;
+import java.sql.Timestamp;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.TreeMap;
 
 public class ReadInputFiles {
 
-	private int counter = 0;
 	private static final String DOC_END_TAG = "</DOCNO>";
 	private static final String DOC_START_TAG = "<DOCNO>";
 	private static final String TEXT_END_TAG = "</TEXT>";
-
 	private static final String TEXT_START_TAG = "<TEXT>";
 
-	// private ReadInputDatatype readInputDatatype;
-	private Set<String> content = new HashSet<>();
-	private List<String> stopList = new ArrayList<>();
-	private String line = null;
-	private int index = 0;
-	private HashMap<Integer, String> wordDict = new HashMap<>();
-	private HashMap<Integer, String> fileDict = new HashMap<>();
+	private List<String> stopList;
+	private String line;
+	private int index, counter;
+	private Map<Integer, String> wordDict;
+	private Map<Integer, String> fileDict;
+	private Porter porter;
+	// private TreeSet<String> sortedTokens;
+	private String stemmedWord;
+	// private Iterator<Entry<String, Map<String, Integer>>> mapIt;
+	private Map<String, Map<String, Integer>> frwdIndex;
+	private Map<String, Integer> termIndex;
+
+	public ReadInputFiles() {
+		super();
+		counter = 0;
+		index = 0;
+		stopList = new ArrayList<>();
+		line = null;
+		wordDict = new TreeMap<>();
+		fileDict = new HashMap<>();
+		porter = new Porter();
+		// sortedTokens = new TreeSet<>(Collator.getInstance());
+		// forwardIndex = new HashMap<>();
+		frwdIndex = new HashMap<>();
+		termIndex = new TreeMap<>();
+
+	}
 
 	// Test
 	public static void main(String args[]) {
-		System.out.println("Started");
+		System.out.println("Started at " + new Timestamp(System.currentTimeMillis()));
 		ReadInputFiles readInputFiles = new ReadInputFiles();
+		if (readInputFiles.stopList.isEmpty()) {
+			readInputFiles.stopList = readInputFiles.loadStopList();
+		}
 		readInputFiles.loadData();
+		// readInputFiles.mapIt =
+		// readInputFiles.frwdIndex.entrySet().iterator();
+		//
+		// while (readInputFiles.mapIt.hasNext()) {
+		// System.out.println(readInputFiles.mapIt.next());
+		// }
+		// for (String string : readInputFiles.sortedTokens) {
+		// if (!string.isEmpty()) {
+		// readInputFiles.wordDict.put(++readInputFiles.index, string);
+		// }
+		// }
+		readInputFiles.writeToFile(readInputFiles);
+		System.out.println("Completed at " + new Timestamp(System.currentTimeMillis()));
 
+	}
+
+	private void writeToFile(ReadInputFiles readInputFiles) {
 		BufferedWriter writer = null;
 		try {
 			writer = new BufferedWriter(new FileWriter("./parser_output.txt"));
@@ -49,43 +84,46 @@ public class ReadInputFiles {
 			writer.flush();
 			writer.close();
 		} catch (Exception e) {
-			// TODO: handle exception
+			e.printStackTrace();
 		}
-		System.out.println("Completed");
-
 	}
 
-	private void writeContent(HashMap<Integer, String> hashMap, BufferedWriter writer) {
+	/**
+	 * Method to write the content to the output file.
+	 * 
+	 * @param mapToPrint
+	 *            hash map write onto a file.
+	 * @param writer
+	 *            buffered writer object.
+	 */
+	private void writeContent(Map<Integer, String> mapToPrint, BufferedWriter writer) {
 
 		try {
 			writer.write("-------------------------------------------");
 			writer.newLine();
-			for (Entry<Integer, String> entry : hashMap.entrySet()) {
+			for (Entry<Integer, String> entry : mapToPrint.entrySet()) {
 				writer.write(entry.getValue() + " " + entry.getKey());
 				writer.newLine();
 			}
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
 	/**
 	 * 
-	 * @return list of lists containing the content from the files.
+	 * Method call to load all the files
 	 */
 	public void loadData() {
 		List<String> filesList = loadFiles("./src/ft911/");
 
 		for (String fileName : filesList) {
-			readFileContent(fileName);
+			processFileContent(fileName);
 		}
-
-		tokenizeContent(content);
-
 	}
 
 	/**
+	 * Method to load the files present in a folder.
 	 * 
 	 * @param path
 	 *            to the folder containing the files.
@@ -103,6 +141,10 @@ public class ReadInputFiles {
 
 	}
 
+	/**
+	 * 
+	 * @return list containing the stop words
+	 */
 	private List<String> loadStopList() {
 		BufferedReader reader = null;
 		try {
@@ -120,11 +162,12 @@ public class ReadInputFiles {
 	}
 
 	/**
+	 * Method to process the file content.
 	 * 
 	 * @param fileToRead
 	 * @return list containing the data.
 	 */
-	private void readFileContent(String fileToRead) {
+	private void processFileContent(String fileToRead) {
 		BufferedReader bufferedReader = null;
 		String docNumber = null;
 		String textLine = null;
@@ -136,28 +179,37 @@ public class ReadInputFiles {
 				if (line.contains(DOC_START_TAG)) {
 					docNumber = line.substring(line.indexOf(DOC_START_TAG) + DOC_START_TAG.length(),
 							line.indexOf(DOC_END_TAG));
-					// if (readInputDatatype == null) {
-					// readInputDatatype = new ReadInputDatatype();
-					// }
-					// readInputDatatype.setIndex(++counter);
-					// readInputDatatype.setmDocId(docNumber);
 					fileDict.put(++counter, docNumber);
 
 				} else if (line.contains(TEXT_START_TAG)) {
+					termIndex = new HashMap<>();
+
 					while (!(textLine = bufferedReader.readLine()).contains(TEXT_END_TAG)) {
-						content.add(" " + textLine);
-						// readInputDatatype.setmDocText(readInputDatatype.getmDocText()
-						// + " " + textLine);
+						for (String token : textLine.toLowerCase().replaceAll("\\w*\\d\\w*", "").trim()
+								.split("\\s*[^a-z]\\s*")) {
+							if (!token.isEmpty() && !stopList.contains(token)) {
+								stemmedWord = porter.stripAffixes(token.trim());
+								// sortedTokens.add(stemmedWord);
+								wordDict.put(++index, stemmedWord);
+								// Doc 1 - cow 2; moon 4; sum 10;
+								// inside Doc 1
+								if (!termIndex.isEmpty() && termIndex.containsKey(stemmedWord)) {
+
+									termIndex.put(stemmedWord, termIndex.get(stemmedWord) + 1);
+								} else {
+									termIndex.put(stemmedWord, 1);
+								}
+
+							}
+						}
+					}
+
+					if (!frwdIndex.containsKey(docNumber)) {
+
+						frwdIndex.put(docNumber, termIndex);
 					}
 				}
-				/*
-				 * if (readInputDatatype != null && counter > 0 &&
-				 * readInputDatatype.getmDocId() != null &&
-				 * readInputDatatype.getmDocText() != null &&
-				 * !readInputDatatype.getmDocText().equals("")) {
-				 * readinputDataList.add(readInputDatatype); readInputDatatype =
-				 * new ReadInputDatatype(); }
-				 */
+
 			}
 
 		} catch (FileNotFoundException e) {
@@ -173,57 +225,4 @@ public class ReadInputFiles {
 		}
 	}
 
-	/**
-	 * 
-	 * @param content
-	 *            total word set that has to be tokenized
-	 */
-	public void tokenizeContent(Set<String> content) {
-
-		Set<String> tokens = new HashSet<>();
-		// Load stop list if its not yet loaded.
-		if (stopList.isEmpty()) {
-			stopList = loadStopList();
-		}
-
-		for (String data : content) {
-			// Removing strings with numbers and then considering words
-			// without punctuation.
-			tokens.addAll(
-					Arrays.asList((data.toLowerCase().replaceAll("\\w*\\d\\w*", "").trim().split("\\s*[^a-z]\\s*"))));
-		}
-
-		tokens = checkStopList(tokens);
-
-		stemWords(tokens);
-	}
-
-	private void stemWords(Set<String> tokens) {
-		Porter porter = new Porter();
-		TreeSet<String> sortedTokens = new TreeSet<String>(Collator.getInstance());
-		for (String stemWords : tokens) {
-			if (!stemWords.isEmpty()) {
-				sortedTokens.add(porter.stripAffixes(stemWords.trim()));
-			}
-		}
-
-		for (String string : sortedTokens) {
-			if (!string.isEmpty()) {
-				wordDict.put(++index, string);
-			}
-		}
-
-	}
-
-	/**
-	 * 
-	 * @param tokens
-	 *            The list of words after dividing into tokens.
-	 * @return
-	 */
-	public Set<String> checkStopList(Set<String> tokensTOCheck) {
-		tokensTOCheck.removeAll(stopList);
-		return tokensTOCheck;
-
-	}
 }
